1e4984cb1f70e16eae39e2914a3968be6d3e37de
zhangxu <329718488@qq.com>
2020-08-09 15:37:23 +0800

    http

---
+++ node/01day.js
@@ -0,0 +1,10 @@
+var a=4;
+var b=7;
+console.log(a+b)
+
+//node name.js  可以运行node文件
+//node 模块 
+// 1.核心模块 ---自带
+// 2.三方模块 ---需下载
+// 3.自定义模块
+
---
+++ node/readFile.js
@@ -0,0 +1,11 @@
+const fs = require("fs")
+
+//  fs.readFile("./test.txt","utf-8",(err,res)=>{
+//     console.log(err)
+//     console.log(res)
+//  })
+
+//  fs.writeFile("./test1.txt","新家的数据喀喀喀",(err,data)=>{
+//     console.log(err)
+//     console.log(data)
+//  })
